a0389e5e07836453e77723472c4ee3788ded9583
Bom <11377ny@gmail.com>
2022-05-18 12:40:37 -0400

    Fix UpdateBoard

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ function updateBoard() {
     if (localStorage.getItem("score")) {
         scr = localStorage.getItem("score")
     } else {
-        scr = 0
+        scr = "0"
     }
     displayScore.innerHTML = `<h3>Score: <span>${scr}</span></h3>`
     moves.innerHTML = `<h3>Your Moves: <span>${move}</span> | Minimum Moves: <span>${minimum}</span></h3>`
